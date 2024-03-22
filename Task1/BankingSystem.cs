8e683c7220af2b9143f4e2f361a1559608f14c1e
JulieDelts <juliadelcova@gmail.com>
2024-03-23 00:31:20 +0300

    Add project files.

---
+++ Task1/BankingSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class BankingSystem
+    {
+        public static bool IsOfferAvailable(int age, int numberOfServicesUsed, double deposit)
+        {
+            if (age >= 60 || deposit > 1000000 || (numberOfServicesUsed >= 3 && deposit > 400000)) return true;
+            return false;
+        }
+        public static void DisplayInfoAboutOfferAvailability(string clientName, bool isOfferAvailable)
+        {
+            if (isOfferAvailable) Console.WriteLine($"Клиент {clientName} может воспользоваться акционным предложением.");
+            else Console.WriteLine($"Клиент {clientName} не может воспользоваться акционным предложением.");
+        }
+
+        public static double CalculateTotalDeposit(double deposit, int duration, double interestRate)
+        {
+            interestRate = interestRate / 100;
+            for (int i = 0; i < duration; i++)
+            {
+                deposit += deposit * interestRate;
+            }
+
+            return deposit;
+        }
+        public static void DisplayInfoAboutDeposit(string clientName, double deposit)
+        {
+            Console.WriteLine($"Размер банковского вклада клиента {clientName} равен {deposit}.");
+        }
+
+        public static int CalculateNumberOfBanknotes(int amountOfMoney)
+        {
+            int thousand = 0;
+            int fiveHundred = 0;
+            int oneHundred = 0;
+
+            while (amountOfMoney >= 1000)
+            {
+                thousand++;
+                amountOfMoney -= 1000;
+            }
+            while (amountOfMoney >= 500)
+            {
+                fiveHundred++;
+                amountOfMoney -= 500;
+            }
+            while (amountOfMoney >= 100)
+            {
+                oneHundred++;
+                amountOfMoney -= 100;
+            }
+
+            return thousand+fiveHundred+oneHundred;
+        }
+        public static void DisplayAtmInfo(int numberOfBanknotes)
+        {
+            Console.WriteLine($"Банкоматом будет произведена выдача {numberOfBanknotes} купюр.");
+        }
+
+    }
+
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Task1;
+
+//использование методов библиотеки 
+
+Console.WriteLine("Введите информацию о клиенте (имя, возраст, количество банковских продуктов, сумма средств на счете):");
+string clientName = Console.ReadLine();
+int age = Convert.ToInt32(Console.ReadLine());
+int numberOfServicesUsed = Convert.ToInt32(Console.ReadLine());
+double deposit = Convert.ToDouble(Console.ReadLine());
+bool isOfferAvailable =  BankingSystem.IsOfferAvailable(age, numberOfServicesUsed, deposit);
+BankingSystem.DisplayInfoAboutOfferAvailability(clientName, isOfferAvailable);
+
+Console.WriteLine();
+
+Console.WriteLine("Введите информацию о имени клиента, размере вклада, продолжительности вклада в годах и процентной ставке:");
+string anotherClientName = Console.ReadLine();
+double anotherDeposit = Convert.ToDouble(Console.ReadLine());
+int duration = Convert.ToInt32(Console.ReadLine());
+double interestRate =  Convert.ToDouble(Console.ReadLine());
+double totalDeposit = BankingSystem.CalculateTotalDeposit(anotherDeposit, duration, interestRate);
+BankingSystem.DisplayInfoAboutDeposit(anotherClientName, totalDeposit);
+
+Console.WriteLine();
+
+Console.WriteLine("Введите сумму денег:");
+int amountOfMoney = Convert.ToInt32(Console.ReadLine());
+int numberOfBanknotes = BankingSystem.CalculateNumberOfBanknotes(amountOfMoney);
+BankingSystem.DisplayAtmInfo(numberOfBanknotes);
